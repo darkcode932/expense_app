d01316932fee269bdb24f8f4d494062c917f139f
darkcode932 <atebederussel@gmail.com>
2022-09-14 23:00:04 +0100

    Interpolation dollar sign string

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class MyHomePage extends StatelessWidget {
                       decoration: BoxDecoration(
                           border: Border.all(color: Colors.orange, width: 2)),
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}',
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
