6458373e9b50b257807be83414e8bd8ec4fa29cb
darkcode932 <atebederussel@gmail.com>
2022-09-16 03:02:37 +0100

    Add New transaction done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import './transaction.dart';
+
+import 'models/transaction.dart';
+import './widgets/user_transactions.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,18 +25,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'Plat Okok', amount: 500, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'Plat Eru', amount: 1000, date: DateTime.now()),
-    Transaction(
-        id: 't3', title: 'Plat Sanga', amount: 2000, date: DateTime.now()),
-  ];
-
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
-
   //String? titleInput;
   //String? amountInput;
 
@@ -52,81 +42,11 @@ class MyHomePage extends StatelessWidget {
             width: double.infinity,
             child: Card(
               color: Colors.blue,
-              child: Text('Hello World'),
+              child: Text('CHART'),
               elevation: 5,
             ),
           ),
-          Card(
-              elevation: 5,
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
-                      //onChanged: (val) {
-                      //titleInput = val;
-                      //},
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-                      //onChanged: (val) {
-                      //amountInput = val;
-                      //},
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        print(titleController.text);
-                        //print(titleInput);
-                        //print(amountInput);
-                      },
-                      child: Text('Add Transaction'),
-                      style: TextButton.styleFrom(primary: Colors.orange),
-                    )
-                  ],
-                ),
-              )),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.orange, width: 2)),
-                      child: Text(
-                        '\$${tx.amount}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.orange),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              );
-            }).toList(),
-          )
+          UserTransactions(),
         ],
       ),
 
